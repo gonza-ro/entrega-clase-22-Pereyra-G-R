9b94e8558ac4d2af31afa2a4ab92f9ad1ba13250
gonza-ro <gonzalorp12345@users.noreply.github.com>
2023-02-09 13:15:20 -0300

    commit-02

--- index.js
+++ index.js
@@ -1,56 +1,47 @@
 import express from 'express'
 import {faker} from '@faker-js/faker'
-/* console.clear(); */
 
-
-const app = express();
-
-
-faker.locale = 'es'
-
-
-
+// Configuramos el idioma español
+faker.locale = 'en'
+const app = express()
 
 let id = 1
 function getNextId(){
     return id++
 }
 
-//-------------------------------------------------------------------
-
-
-function ProductosFaker(id) {
- 
-return {
-    id, 
-    nombre: faker.name.firstName(),
-    precio: faker.finance.amount(),
-    foto: faker.image.avatar()
-}
+// Vamos a crear una función que nos traiga elementos de forma aleatoria
+function crearAlAzar(id) {
+    return {
+        id,
+        nombre: faker.name.firstName(),
+        precio: faker.finance.amount(),
+        foto: faker.image.avatar()
+    }
 }
 
-function generarProductos(cant){
-    const productos = [];
-    for( let i = 0; i=cant; i++ ){
-    productos.push(ProductosFaker(getNextId))
+function generarProductos(cant) {
+    const productos = []
+    for(let i = 0; i < cant; i++) {
+        productos.push(crearAlAzar(getNextId()))
+    }
+    return productos
 }
 
-    return productos;
-}
 
-const cant_productos = 5;
+
+// crear variable de entorno
+const CANT_PRODUCTOS = 5
 
 app.get('/api/productos', (req, res) => {
-    const cant = Number(req.query.cant) || cant_productos
+    const cant = Number(req.query.cant) || CANT_PRODUCTOS
     res.json(generarProductos(cant))
     console.log(generarProductos(cant))
 })
 
-//-------------------------------------------------------------------
-
-/* Configurar el servidor */
-const PORT = 8080;
-const server = app.listen(PORT, () => {
-    console.log('Server ON, in port: ', PORT);
+// Configuramos nuestro server
+const PORT = 8080
+const srv = app.listen(PORT, () => {
+    console.log('Serer ON, in port: ' + PORT)
 })
-server.on('error', error => console.log('Error in Server: ', error));
+srv.on('error', error => console.log('Error en el servidor: ' + PORT))
